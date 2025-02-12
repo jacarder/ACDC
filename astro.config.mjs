74b5ab383e3c7771a6471d4b89329e5cd2b07519
Jacarder <jonandrewcarder@gmail.com>
2025-02-11 20:21:43 -0500

    Add shadcn/ui and fix vite resolve paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,10 +4,20 @@ import { defineConfig } from "astro/config";
 import sanity from "@sanity/astro";
 import react from "@astrojs/react";
 import tailwindcss from "@tailwindcss/vite";
+import path from 'path'
+
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/',
   vite: {
+    resolve: {
+      alias: {
+        // your aliases should mirror the ones defined in your tsconfig.json
+        // i'll re-use the ones in the official docs:
+        "@/*": path.resolve(path.dirname(''), './src/*'),
+      }
+    },
     plugins: [tailwindcss()]
   },
   integrations: [
